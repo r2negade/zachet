343620e4af388d520229f09b79108e8db301bc72
r2negade <154789374+r2negade@users.noreply.github.com>
2023-12-25 09:48:06 +0300

    Add files via upload

---
+++ script/script.js
@@ -0,0 +1,12 @@
+document.getElementById('vkButton').addEventListener('click', function() {
+    window.location.href = 'https://vk.com/irenegadei';
+});
+
+document.getElementById('okButton').addEventListener('click', function() {
+    window.location.href = 'https://ok.ru/123';
+});
+
+document.getElementById('youtubeButton').addEventListener('click', function() {
+    window.location.href = 'https://youtube.com';
+});
+
